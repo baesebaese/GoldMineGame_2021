922b28bd8d04ed344b0cd0ba44704ca65c1d1997
Seeun Bae <morendo90@gmail.com>
2021-12-21 23:00:07 +0900

    Make image

--- 2_background.py
+++ 2_background.py
@@ -1,14 +1,18 @@
-#기본 뼈대 생성
+# 배경 이미지 설정
 
 import pygame
+import os
 
 pygame.init()
 screen_width = 1280
 screen_hight = 720
 screen = pygame.display.set_mode((screen_width, screen_hight))
 pygame.display.set_caption("Gold Miner")
-
 clock = pygame.time.Clock()
+
+# 배경 이미지 불러오기
+current_path = os.path.dirname(__file__) # 현재 파일의 위치 반환
+background = pygame.image.load(os.path.join(current_path, "background.png"))
 
 running = True
 while running:
@@ -18,4 +22,7 @@ while running:
         if event.type == pygame.QUIT:
             ruuning = False
     
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+
 pygame.quit()
